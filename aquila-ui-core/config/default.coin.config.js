cc714bb8bcd926e3d18ea192b2010e715e86a6f7
SahkanDesertHawk <28378150+SahkanDesertHawk@users.noreply.github.com>
2023-03-16 20:35:49 -0400

    Add files via upload

---
+++ aquila-ui-core/config/default.coin.config.js
@@ -0,0 +1,11 @@
+const coin = {
+    name: 'Aquila',
+    symbol: 'UNCIA',
+    addressCount: 1,
+    addressVersion: 23,
+    decimals: 100000000,
+    logo: '/img/UNCIA_LOGO.png',
+    icon: '/img/UNCIA_LOGO.png'
+}
+
+module.exports = coin
